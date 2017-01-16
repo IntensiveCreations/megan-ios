a34a6ff63a3248a360b1754d636eb04acd785d97
Tony Lefebvre <tony.lbvre@gmail.com>
2017-01-16 11:56:02 +0100

    Adds init files

---
+++ megan/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  megan
+//
+//  Created by tony on 1/10/17.
+//  Copyright © 2017 megan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
